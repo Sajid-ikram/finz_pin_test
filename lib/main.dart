e3a97b3ebab4d3d98bd6c6901992620cee0c5d53
Sajid-ikram <sajed.ikram@yahoo.com>
2025-05-07 16:47:37 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,122 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late InAppWebViewController _controller;
+
+  InAppWebView _buildWebview() {
+    return InAppWebView(
+      onWebViewCreated: (webViewController) {
+        _controller = webViewController;
+
+        _controller.loadFile(assetFilePath: "assets/pin_dev.html");
+      },
+      initialOptions: InAppWebViewGroupOptions(
+        crossPlatform: InAppWebViewOptions(
+          useShouldOverrideUrlLoading: true,
+          mediaPlaybackRequiresUserGesture: false,
+        ),
+        android: AndroidInAppWebViewOptions(useHybridComposition: true),
+        ios: IOSInAppWebViewOptions(allowsInlineMediaPlayback: true),
+      ),
+      onProgressChanged: (controller, progress) {
+        if (progress == 100) {}
+        print('progress: $progress ---------------------------------------');
+      },
+      onConsoleMessage: (webViewController, consoleMessage) {
+        String message = consoleMessage.message;
+        log('message: $message');
+        if (message.startsWith('token:')) {
+          /*ref.read(pinUiComponentNotifier.notifier).tokenizePinAndSubmit(
+            arguments: widget.passcodeArguments,
+            consoleMessage: consoleMessage,
+          );*/
+          print('message: $message ++++++++++++++++++++++++');
+        }
+      },
+      onReceivedServerTrustAuthRequest: (controller, challenge) async {
+        return ServerTrustAuthResponse(
+          action: ServerTrustAuthResponseAction.PROCEED,
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Flutter Demo Home Page')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Enter your PIN', style: TextStyle(fontSize: 20)),
+            SizedBox(height: 20),
+            Container(
+              height: 50,
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: _buildWebview(),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                padding: EdgeInsets.symmetric(horizontal: 20),
+              ),
+              onPressed: (){
+
+              },
+              child: Text('Submit', style: TextStyle(color: Colors.white),),
+            )
+
+          ],
+        ),
+      ),
+    );
+  }
+}
